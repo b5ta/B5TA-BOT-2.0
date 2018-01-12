0ed63f81ae1fe6a02e8480684e8245905ca071db
APickles <Apickles15@gmail.com>
2018-01-11 22:53:24 -0700

    B5TA Bot 2.0 first Commit

---
+++ commands/PVM/chaoselemental.js
@@ -0,0 +1,18 @@
+exports.info = {
+  name: "Chaos Elemental",
+  category: "Info",
+  aliases: ["chaos", "chaos elemental"],
+  description: "The Chaos Elemental dwells in the members-only section of the Wilderness, just west of the Rogues Castle." +
+                          " This area is moderately easy to access from the deep Wilderness teleport lever in Ardougne or Edgeville." +
+                          " A Wilderness Obelisk can randomly teleport players southeast of the Rogues Castle.\n\n",
+  website: "http://runescape.wikia.com/wiki/Chaos_Elemental/Strategies",
+  combatstyle: "Range",
+  armour: "Demon slayer circlet > Royal dragonhide coif\n"+
+          "Saradomins murmur > Amulet of glory\n"+
+          "Demon slayer torso > Royal dragonhide body\n"+
+          "Demon slayer skirt > Royal dragonhide chaps\n"+
+          "Royal spiky vambraces\n"+
+          "Turoth boots\n"+
+          "Max cape > Skill cape\n\n",
+  weapon: "Crystal bow/Royal crossbow > Black salamander\n"
+};
